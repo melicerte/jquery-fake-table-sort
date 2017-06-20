49d8f78347eb8d811fdd6c1732e37567a72539c6
melicerte <etienne.chataignier@gmail.com>
2017-06-20 16:53:25 +0200

    First release

--- README.md
+++ README.md
@@ -1,2 +1,88 @@
-# jquery-tablefake-sort
-Sort fake tables
+# Simple fake table sorter
+
+This jQuery plugin allows to sort table made without table markup.
+
+## Demo
+
+<a href="https://melicerte.github.io/jquery-faketable-sort/demo.html" target="_blank">jQuery fake table sort demo</a>
+
+## Installation
+
+Make sure to include jQuery in your page:
+
+```html
+<script
+  src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+  integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
+  crossorigin="anonymous"></script>
+```
+
+Include **jQuery Fake Table Sort:**
+
+```html
+<script src="js/jquery.fakeTableSort.min.js"></script>
+```
+
+### Prerequisites
+
+```
+jQuery >= 1.7.0
+```
+
+## Usage
+
+```javascript
+jQuery('.table-fake').fakeTableSortable();
+```
+
+### With options
+```javascript
+jQuery('.table-fake').fakeTableSortable({
+    headerItems: 'div.table-fake-row-first > div',
+    lineItems: 'div.table-fake-row',
+    cellItems: 'div.table-fake-col',
+    sortMethods: ['lexicographical', 'lexicographical', 'lexicographical', 'number'],
+    textConverter: [null, null, convertDateInterval, null]
+});
+```
+
+**headerItems** : CSS selector to find headers cells in the fake table.<br/>
+*Defaults to div.table-fake-row-first > div*
+
+**lineItems** : CSS selector to find content lines in the fake table.<br/>
+*Defaults to div.table-fake-row*
+
+**cellItems** : CSS selector to find content cell in content lines<br/>
+*Defaults to div.table-fake-col*
+
+**firstSort** : 'asc' or 'desc' : set first wanted order sort.<br/>
+ Defaults to 'asc'.
+
+**sortMethods** : Used to choose the sorting method.<br/>
+Can be either a string or an array.<br/>
+If it is string, the same sort method is used for every column.<br/>
+If it is an array, each value will be used for one column.<br/>
+You can use 'lexicographical' or 'number' sort.<br/>
+*Defaults to "lexicographical"*
+
+**textConverter** : Used to convert cell content before comparing for sort.<br/>
+Can be either a function callback or an array of callback functions.<br/>
+If it is string, the text converter callback is used for every column.<br/>
+If it is an array, each callback will be used for its associated column.<br/>
+The callback function must take one argument **value**, the value to convert, and return the converted value.<br/>
+*Defaults to null*
+
+### Style
+
+You can add style on links added on header items.
+
+They have class **fake-table-sorter**, plus **asc** if next action is to sort asc, **desc** id next action is desc sort
+
+## Implementation details
+
+This plugin uses the quicksort algorithm (Lomuto partition scheme).
+
+
+## Authors
+
+* **Etienne Châtaignier** - *First release*
---
+++ docs/jquery.fakeTableSort.js
@@ -0,0 +1,215 @@
+(function ( $ ) {
+    'use strict';
+
+    $.fn.fakeTableSortable = function (options) {
+
+        var settings = $.extend({}, $.fn.fakeTableSortable.defaults, options);
+
+        return this.each(function() {
+            var $table = $(this);
+            initHeaders($table, settings);
+        });
+
+    };
+
+    /**
+     * Add links on header items
+     * @param $table
+     * @param settings
+     */
+    function initHeaders ($table, settings) {
+
+        $table.find(settings.headerItems).each(function(){
+            var $headerItem = $(this);
+
+            var $action = $('<a href="#" rel="sort" class="fake-table-sorter ' + settings.firstSort + '"></a>').on('click', function(e){
+                var aSort = $(this);
+                sort(aSort, settings, $table);
+
+                if (aSort.hasClass('asc')) {
+                    aSort.removeClass('asc').addClass('desc');
+                } else {
+                    aSort.removeClass('desc').addClass('asc');
+                }
+
+                return false;
+            });
+
+            $action.append($headerItem.text());
+
+            $headerItem.html($action);
+        });
+
+    };
+
+    /**
+     * Do sort on selected column
+     * @param elt
+     * @param settings
+     * @param $table
+     * @returns {boolean}
+     */
+    function sort (elt, settings, $table) {
+
+        //Get column datas
+        var $column     = elt.parent();
+        var $columnList = $column.parent().children();
+        var iCol        = $columnList.index($column);
+        var columns     = [];
+
+        var $lineItems = $table.find(settings.lineItems);
+
+        //For each line, get cell texts to use for sort
+        $lineItems.each(function(){
+
+            var line = $(this);
+
+            var cell = $(line.find(settings.cellItems).get(iCol));
+
+            var cellText = cell.text().trim();
+            if (typeof settings.textConverter === 'string') {
+                cellText = settings.textConverter(cellText);
+            }
+
+            if (typeof settings.textConverter === 'object' && settings.textConverter !== null && settings.textConverter[iCol] != null) {
+                cellText = settings.textConverter[iCol](cellText);
+            }
+
+            columns.push({
+                line: line,
+                text: cellText
+            });
+        });
+
+        //Set sort method
+        var sortMethod = 'lexicographical';
+        switch(typeof settings.sortMethods) {
+            case 'string':
+                sortMethod = settings.sortMethods;
+                break;
+            case 'object':
+                sortMethod = settings.sortMethods[iCol];
+                break;
+        }
+
+        //Start quicksort algorithm
+        quickSort(columns, 0, columns.length - 1, sortMethod);
+
+        //Reverse sort if wanted
+        if (elt.hasClass('desc')) {
+            columns.reverse();
+        }
+
+        //Delete lines from fake table
+        $lineItems.remove();
+
+        //Add sorted lines to fake table
+        for (var iLine = 0; iLine < columns.length ;iLine++) {
+
+            var curLineItems = $table.find(settings.lineItems);
+            var $line = columns[iLine].line;
+
+            //By default, add after header
+            var $where = $table.find(settings.headerItems).parent();
+
+            //If lines already added, add after last line
+            if (curLineItems.length > 0) {
+                $where = curLineItems.last();
+            }
+
+            //Do add line
+            $line.insertAfter($where);
+        }
+
+        return false;
+    };
+
+    /**
+     * Quicksort algorithm
+     * @param array
+     * @param low
+     * @param high
+     */
+    function quickSort (array, low, high, sortMethod) {
+        if (low < high) {
+            var p = partition(array, low, high, sortMethod);
+            quickSort(array, low, p - 1, sortMethod);
+            quickSort(array, p + 1, high, sortMethod);
+        }
+    };
+
+    /**
+     * partition function for the Quicksort algorithm
+     * @param array
+     * @param low
+     * @param high
+     * @param sortMethod
+     * @returns {number}
+     */
+    function partition (array, low, high, sortMethod) {
+        var pivot = array[high];
+        var i = low -1;
+
+        for (var j = low; j <= high; j++) {
+
+            var comparison = compareValues(array[j].text, pivot.text, sortMethod);
+
+            if (comparison === 0 || comparison === -1) {
+                i++;
+                if (i != j) {
+                    var temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        return i;
+    };
+
+    /**
+     * Compare 2 values according to $method
+     * @param $value1
+     * @param $value2
+     * @param $method
+     * @returns {number} -1 if $value1 < $value2, 0 if $value1 = $value2, 1 if $value1 > $value2
+     */
+    function compareValues ($value1, $value2, $method) {
+        switch ($method) {
+            case 'lexicographical':
+                return $value1.localeCompare($value2);
+                break;
+            case 'number':
+                $value1 = parseFloat($value1);
+                $value2 = parseFloat($value2);
+
+                if ($value1 < $value2) {
+                    return -1;
+                }
+                if ($value1 == $value2) {
+                    return 0;
+                }
+                if ($value1 > $value2) {
+                    return 1;
+                }
+                break;
+        }
+
+        return 0;
+    };
+
+
+    /**
+     * Default options for jQuery.FakeTableSort
+     * @type {{headerItems: string, lineItems: string, cellItems: string, firstSort: string, sortMethods: string, textConverter: null}}
+     */
+    $.fn.fakeTableSortable.defaults = {
+        headerItems: 'div.table-fake-row-first > div',  //Header items
+        lineItems: 'div.table-fake-row',                //Line items
+        cellItems: 'div.table-fake-col',                //Column items
+        firstSort: 'asc',                               //First sort order : asc, then desc, then asc, etc.
+        sortMethods: 'lexicographical',                 //Sort method : lexicographical for lexicographical, number for numbers. For a different sort function on each column, use ['number', 'lexicographical']
+        textConverter: null                             //Cell text converter : convert data of cell before sorting. Can be string or array with same number of items as sortMethods parameter
+    };
+
+}(jQuery || this.jQuery || window.jQuery));
